9dff8942fa035ea375e840e75f1b110aa8aa09dc
Michael <michael.philipe2013@gmail.com>
2023-03-28 02:36:21 -0300

    link sobre feito

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <a href='#to-do' className='action'>
+      Sobre
+    </a>
+  );
 }
 
 export default App;
