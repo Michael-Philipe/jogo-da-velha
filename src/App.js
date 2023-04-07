d2f5cee1f60afb6e6616bf54f57800563833c757
Michael <michael.philipe2013@gmail.com>
2023-04-07 01:47:16 -0300

    Criou o component Header sem o CSS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import Logo from './objects/Logo';
 import Sobre from './objects/Sobre';
 import Menu from './objects/Menu';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <Logo />
-      <Sobre />
-      <Menu />
+      <Header />
     </>
   );
 }
